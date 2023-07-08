f3c745ccf4f23b6b1921e43176a61bd35f30b774
maivu <vungoc2901@gmail.com>
2023-07-09 00:50:40 +0700

    init project

---
+++ android/app/src/main/kotlin/vn/com/rangdong/ework/ework/MainActivity.kt
@@ -0,0 +1,6 @@
+package vn.com.rangdong.ework.ework
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/config/themes/app_colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/widgets.dart';
+
+/// Application-wide constants. Split into different areas: theme, layout and
+class AppColors {
+  /// Primary color of the app.
+  static const Color primaryColor = Color(0xffffdb00);
+}
